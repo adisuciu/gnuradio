aef1d7372a047e6d057cd05314fdfbe666d64c03
Adrian Suciu <adrian.suciu@analog.com>
2020-02-20 18:32:19 +0200

    Updated appveyor.yml
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- build.sh
+++ build.sh
@@ -37,6 +37,7 @@ DEPENDENCIES="mingw-w64-${ARCH}-libxml2 \
 	mingw-w64-${ARCH}-glibmm \
 	mingw-w64-${ARCH}-pkg-config \
 	python3-mako \
+	python-six 
 	" \
 
 # Remove dependencies that prevent us from upgrading to GCC 6.2
